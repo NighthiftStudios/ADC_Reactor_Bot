976fb4cd88000f4d4c43190f263c994d37fb6f0e
24GFOX <34461285+24GFOX@users.noreply.github.com>
2024-05-12 00:20:45 -0500

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,11 +5,11 @@ module.exports = {
 	name: Events.MessageCreate,
 	async execute(config, message) {
 		if (message.author.bot) return;
-        if (message.channel.id != config.reactionChannel) return;
-        if (message.member.roles.cache.some(role => config.exemptRoles.includes(role.id))) return;
-        if (config.exemptUsers.includes(message.author.id)) return;
-        await message.react(config.proEmoji)
-        await message.react(config.conEmoji)
-        await message.startThread({ "name": message.author.username + "'s Suggestion"})
+	        if (message.channel.id != config.reactionChannel) return;
+	        if (message.member.roles.cache.some(role => config.exemptRoles.includes(role.id))) return;
+	        if (config.exemptUsers.includes(message.author.id)) return;
+	        await message.react(config.proEmoji)
+	        await message.react(config.conEmoji)
+	        await message.startThread({ "name": message.author.username + "'s Suggestion"})
 	},
 };
